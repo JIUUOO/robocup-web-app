9da5e50a47cfe78cc343ad49d417592761d5436c
JIUUOO <nninjiuuoo@gmail.com>
2024-05-17 16:33:58 +0900

    feat: add tailwind dependency

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,3 +1,11 @@
+import rckaLogo from "../../images/logo/rcka-logo.png";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-center items-center py-8">
+      <div>
+        <img className="" src={rckaLogo}></img>
+      </div>
+    </nav>
+  );
 }
