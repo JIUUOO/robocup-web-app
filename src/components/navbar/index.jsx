bdf836843e1e97643aa12a06c5ca652555911cc5
JIUUOO <nninjiuuoo@gmail.com>
2024-05-19 05:35:19 +0900

    fix: uninstall bootstrapC

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,13 +2,5 @@ import rckaLogo from "../../images/logo/rcka-logo.png";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <nav className="flex items-center h-16 mx-auto px-5 shadow-md ">
-      <div>
-        <NavLink to={"/"}>
-          <img className="h-14" src={rckaLogo}></img>
-        </NavLink>
-      </div>
-    </nav>
-  );
+  return <div className="container text-center text-primary">he</div>;
 }
