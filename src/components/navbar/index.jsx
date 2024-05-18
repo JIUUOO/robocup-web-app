6f03dff768c2e1f56e961cf30c8cc2a4910f6575
JIUUOO <nninjiuuoo@gmail.com>
2024-05-19 05:18:37 +0900

    feat: remove tailwindcss then add bootstrap

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,13 @@
 import rckaLogo from "../../images/logo/rcka-logo.png";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-center items-center py-8">
+    <nav className="flex items-center h-16 mx-auto px-5 shadow-md ">
       <div>
-        <img className="" src={rckaLogo}></img>
+        <NavLink to={"/"}>
+          <img className="h-14" src={rckaLogo}></img>
+        </NavLink>
       </div>
     </nav>
   );
