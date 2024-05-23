f995bd7a6cf065b2986946d07a271bfad3c586fd
JIUUOO <nninjiuuoo@gmail.com>
2024-05-23 14:51:03 +0900

    design: add font

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
 import "./assets/styles/app.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
