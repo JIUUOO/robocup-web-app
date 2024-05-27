6852253deb28ab4c7925ffe28776a313ec463818
JIUUOO <nninjiuuoo@gmail.com>
2024-05-27 15:48:30 +0900

    design: add breakpoints for fs-6

--- src/pages/games/soccer/index.jsx
+++ src/pages/games/soccer/index.jsx
@@ -9,18 +9,22 @@ import { NavLink } from "react-router-dom";
 export default function Soccer() {
   return (
     <Container className="">
-      <h1 className="mb-4 fw-semibold">RoboCup Junior Soccer</h1>
-      <p className="fs-5 mb-4">
-        <span className="d-block">
-          &nbsp;외부의 조작 없이 경기가 진행되며 공을 드리블하여 상대의 골대
-          안으로 넣는 로봇 축구 경기입니다.
-        </span>
-        <span className="d-block">
-          &nbsp;한 팀당 2 대의 로봇을 출전시킬 수 있으며 정해진 로봇 디자인이나
-          프로그램은 없습니다.
-        </span>
-        <span className="d-block">&nbsp;단, 크기와 무게 제한이 있습니다.</span>
-      </p>
+      <div className="shadow p-2 rounded mb-4">
+        <h1 className="mb-4 fw-semibold">RoboCup Junior Soccer</h1>
+        <p className="fs-5">
+          <span className="d-block">
+            &nbsp;외부의 조작 없이 경기가 진행되며 공을 드리블하여 상대의 골대
+            안으로 넣는 로봇 축구 경기입니다.
+          </span>
+          <span className="d-block">
+            &nbsp;한 팀당 2 대의 로봇을 출전시킬 수 있으며 정해진 로봇
+            디자인이나 프로그램은 없습니다.
+          </span>
+          <span className="d-block">
+            &nbsp;단, 크기와 무게 제한이 있습니다.
+          </span>
+        </p>
+      </div>
       <Row>
         <Col lg={6}>
           <h2 className="fw-semibold">Soccer LightWeight</h2>
