73aca23dc710abafe5646ca78b1de555fca6ade9
JIUUOO <nninjiuuoo@gmail.com>
2024-05-27 16:10:48 +0900

    fix: I have no idea why I've used hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/app.css";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
