9853388301338e337d6801afd2648d6252d04697
JIUUOO <nninjiuuoo@gmail.com>
2024-05-30 11:49:17 +0900

    feat: add scroll to top component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header";
+import ScrollToTop from "./components/scrolltotop";
 import Home from "./pages/home";
 import About from "./pages/association/about";
 import Committee from "./pages/association/committee";
@@ -21,6 +22,7 @@ function App() {
     <div className="App">
       <Header />
       <div className="header-padding"></div>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/association/about" element={<About />} />
